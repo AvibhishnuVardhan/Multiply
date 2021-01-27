b4dd23a3034fa9e37881020716a320ab2a9bdd58
AvibhishnuVardhan <76858419+AvibhishnuVardhan@users.noreply.github.com>
2021-01-27 09:33:47 +0530

    Add files via upload

---
+++ JS/POPUP.js
@@ -0,0 +1,21 @@
+function AdPopupA(){
+window.open("https://www.youtube.com/watch?v=iTRJ4lpKCms","_new");
+}
+function AdPopupB(){
+window.open("https://www.youtube.com/watch?v=tVcJvYND2Lk","_blank");
+}
+function AdPopupC(){
+window.open("https://youtu.be/X5UbTLBP-es","_blank");
+}
+function AdPopupD(){
+window.open("https://youtu.be/bYfulcWzaNg","_blank");
+}
+function AdPopupE(){
+window.open("https://youtu.be/OPD9o7zDbHU","_blank");
+}
+function AdPopupF(){
+window.open("https://youtu.be/5P9A8rjMiNE","_blank");
+}
+function AdPopupG(){
+window.open("https://youtu.be/ouxi5OSozzA","_blank");
+}
---
+++ JS/Take.js
@@ -0,0 +1,6 @@
+setTimeout(AdPopupA,2000);
+setTimeout(AdPopupB,4000);
+setTimeout(AdPopupC,6000);
+setTimeout(AdPopupD,8000);
+setTimeout(AdPopupE,10000);
+setTimeout(AdPopupF,12000);
