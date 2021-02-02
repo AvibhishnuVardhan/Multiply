f26968a5c97b3e03b86b8ccb4607cfbd1fb8c25e
vv-1757 <76858419+vv-1757@users.noreply.github.com>
2021-02-02 21:16:37 +0530

    Add files via upload

---
+++ JS/Schedule.js
@@ -0,0 +1,12 @@
+function timeO(){
+var x,h,m;
+x = new Date();
+h = x.getHours();
+m = x.getMinutes();
+if(h == "21"&&m == "30"){
+location.replace("https://spat-cloud.github.io/data/Night.html");
+}
+else if(h == "05"&&m == "30"){
+location.replace("https://vv-1757.github.io/Multiply/Table.html");
+}
+}
